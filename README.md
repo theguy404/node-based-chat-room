b197bdbf7c742e58cf0798022d4e7981bf4b09c4
Terry Campbell <theguy404@gmail.com>
2017-01-28 00:45:00 +0000

    added users list and disconnect message

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # node-based-chat-room
 
 this repo is for building a node based chat room from scratch as a test of skills.
+
+# Current Features:
+
+* Basic chat system
+* user login
+* connect message when users join
+* saves up to 100 previous messages (as long as server is not closed)
+* added users list and disconnect message when users leave
--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var messages = [];
 
-app.get('/', function(req, res){
+var storeMessages = function(name, data) {
+  messages.push({name: name, data: data});
+  if(messages.length > 100){
+    messages.shift();
+  }
+}
+
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    io.emit('chat message', "A user has connected");
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', function(socket) {
+  socket.on('join', function(name) {
+    messages.forEach(function(message) {
+      socket.emit('chat message', message.name + ": " + message.data);
+    });
+
+    socket.nickName = name;
+    socket.broadcast.emit('users', name);
+    Object.keys(io.sockets.sockets).forEach(function(savedSocket) {
+      socket.emit('users', io.sockets.connected[savedSocket].nickName);
+    });
+
+    socket.broadcast.emit('chat message', name + " has connected.");
+    socket.emit('chat message', name + " has connected.");
   });
+
+  socket.on('chat message', function(msg) {
+    socket.broadcast.emit('chat message', socket.nickName + ": " + msg);
+    socket.emit('chat message', socket.nickName + ": " + msg);
+    storeMessages(socket.nickName, msg);
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('remove user', socket.nickName);
+    io.emit('chat message', socket.nickName + " has disconnected");
+  });
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3010, function(){
+  console.log('listening on *:3010');
 });
